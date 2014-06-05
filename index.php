65ad5551a36d0809934ac1a3822f6b372d7397ac
Jennifer Quispe <jennyc_q@jennifeispesmbp.gateway.2wire.net>
2014-06-05 00:59:01 -0400

    Installed bootstrap template

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8" />
-	<title>Jenny Test Site/title>
-</head>
-<body>
- 	<p>Welcome to my site</p>
-</body>
+  <head>
+  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
 </html>
